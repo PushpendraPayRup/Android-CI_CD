e2926a1874a62cbe097a3bdadfcafe1a25c1f2af
Pushpendra <kpushpendra@payrup.com>
2024-03-14 12:00:11 +0530

    Checking Webhook Trigger

--- app/src/main/java/com/example/androidcicd/HomeFragment.kt
+++ app/src/main/java/com/example/androidcicd/HomeFragment.kt
@@ -24,3 +24,50 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
+
+//pipeline {
+//    agent any
+//
+//            environment {
+//                JAVA_HOME = '/Users/admin/Library/Java/JavaVirtualMachines/openjdk-21.0.1/Contents/Home'
+//                ANDROID_HOME = '/Users/admin/Library/Android/sdk'
+//            }
+//
+//    options {
+//        timeout(time: 1, unit: 'HOURS') // Set a timeout for the entire pipeline
+//    }
+//
+//    stages {
+//        stage('Checkout') {
+//            steps {
+//                script {
+//                    // Ensure Gradle wrapper exists and is executable
+//                    sh 'chmod +x gradlew'
+//                    // Checkout your source code from your version control system (e.g., Git)
+//                    git url: 'https://github.com/PushpendraPayRup/Android-CI_CD.git'
+//                }
+//            }
+//        }
+//        stage('Build') {
+//            steps {
+//                script {
+//                    sh './gradlew assembleDebug'
+//                }
+//            }
+//        }
+//        stage('Archive APK') {
+//            steps {
+//                script {
+//                    // Archive the generated APK file
+//                    archiveArtifacts artifacts: '**/*.apk', fingerprint: true
+//                }
+//            }
+//        }
+//    }
+//    post {
+//        always {
+//            // Clean up workspace
+//            cleanWs()
+//        }
+//    }
+//}
